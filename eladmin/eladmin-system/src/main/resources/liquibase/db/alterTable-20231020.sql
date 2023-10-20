fd04f345447c1d9f66cd3def26527fe275ca3ccc
wangzy <wangzy.vip@outlook.com>
2023-10-20 23:36:41 +0800

    [feat] liquibase 测试

---
+++ eladmin/eladmin-system/src/main/resources/liquibase/db/alterTable-20231020.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `eladmin`.`business_test`
+    CHANGE COLUMN `test2` `test-2` VARCHAR(255) NOT NULL COMMENT '账号';
+ALTER TABLE `eladmin`.`business_test`
+    CHANGE COLUMN `test3` `test-3` VARCHAR(255) NOT NULL COMMENT '密码' ;
