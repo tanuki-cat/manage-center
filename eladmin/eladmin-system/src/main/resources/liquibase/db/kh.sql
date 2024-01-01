0de4a9dcc161121a0482ce751e4ed11f73020d97
jgg <756252348@qq.com>
2024-01-01 20:54:00 +0800

    修改首页，添加这些图

--- eladmin/eladmin-system/src/main/resources/liquibase/db/kh.sql
+++ eladmin/eladmin-system/src/main/resources/liquibase/db/kh.sql
@@ -106,3 +106,55 @@ CREATE TABLE `sys_company_ext`
     PRIMARY KEY (`id`),
     UNIQUE KEY `company_id` (`company_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='公司扩展信息表';
+
+-- changeset lychee:20240101_1827
+-- comment 修改项目表添加到期时间
+ALTER TABLE `sys_project`
+    ADD COLUMN `end_time` varchar(255) NULL COMMENT '到期时间' AFTER `project_amount`;
+
+CREATE TABLE `sys_patent`
+(
+    `patent_id`   bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `company_id`   bigint(20) DEFAULT NULL COMMENT '公司id',
+    `company_name` varchar(255)  DEFAULT NULL COMMENT '公司名称',
+    `invention` varchar(255)  DEFAULT NULL COMMENT '发明专利',
+    `utility_model` varchar(255)  DEFAULT NULL COMMENT '实用新型专利',
+    `appearance` varchar(255)  DEFAULT NULL COMMENT '外观专利',
+    `software_works` varchar(255)  DEFAULT NULL COMMENT '软件著作',
+    `copyright` varchar(255)  DEFAULT NULL COMMENT '版权',
+    `progress` varchar(255)  DEFAULT NULL COMMENT '专利进度',
+    `filing_time` varchar(255)  DEFAULT NULL COMMENT '专利申报时间',
+    `write_time` varchar(255)  DEFAULT NULL COMMENT '专利写好时间',
+    `authorization_time` varchar(255)  DEFAULT NULL COMMENT '授权下证时间',
+    `patent_num` varchar(255)  DEFAULT NULL COMMENT '专利数',
+    `forewarn_time` varchar(255)  DEFAULT NULL COMMENT '年费预警时间',
+    `patent_status`  int(8)  DEFAULT NULL COMMENT '状态',
+    `nick_name`  varchar(255)  DEFAULT NULL COMMENT '创建者名字',
+    `create_by`    varchar(255)  DEFAULT NULL COMMENT '创建者',
+    `update_by`    varchar(255)  DEFAULT NULL COMMENT '更新者',
+    `create_time`  datetime      DEFAULT NULL COMMENT '创建日期',
+    `update_time`  datetime      DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`patent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='专利表';
+
+CREATE TABLE `sys_patent_schedule`
+(
+    `schedule_id`   bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `patent_id`   bigint(20) DEFAULT NULL COMMENT '项目id',
+    `schedule_status`  int(8)  DEFAULT NULL COMMENT '进度状态',
+    `assign_user`  varchar(255)  DEFAULT NULL COMMENT '指派人',
+    `assign_user_id` bigint(20) NULL COMMENT '指派人员Id' ,
+    `assign_status` int(2) NOT NULL DEFAULT 0 COMMENT '指派状态 1- 作废 0- 指派',
+    `progress` varchar(255)  DEFAULT NULL COMMENT '专利进度',
+    `filing_time` varchar(255)  DEFAULT NULL COMMENT '专利申报时间',
+    `write_time` varchar(255)  DEFAULT NULL COMMENT '专利写好时间',
+    `authorization_time` varchar(255)  DEFAULT NULL COMMENT '授权下证时间',
+    `patent_num` varchar(255)  DEFAULT NULL COMMENT '专利数',
+    `forewarn_time` varchar(255)  DEFAULT NULL COMMENT '年费预警时间',
+    `nick_name`  varchar(255)  DEFAULT NULL COMMENT '创建者名字',
+    `create_by`    varchar(255)  DEFAULT NULL COMMENT '创建者',
+    `update_by`    varchar(255)  DEFAULT NULL COMMENT '更新者',
+    `create_time`  datetime      DEFAULT NULL COMMENT '创建日期',
+    `update_time`  datetime      DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`schedule_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='项目进度管理';
